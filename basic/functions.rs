64d4f3a4e8d3c523f41bb92db3e0f82210869ebf
simonteozw <simonteozw@gmail.com>
2022-01-01 11:20:45 +0800

    Basic function declarations in rust

---
+++ basic/functions.rs
@@ -0,0 +1,39 @@
+fn sqr1(x: f64) -> f64 {
+    return x * x;
+}
+
+fn sqr2(x: f64) -> f64 {
+    x * x
+}
+
+// absolute value of a floating-point number
+fn abs(x: f64) -> f64 {
+    if x > 0.0 {
+        x
+    } else {
+        -x
+    }
+}
+
+// ensure the number always falls in the given range
+fn clamp(x: f64, x1: f64, x2: f64) -> f64 {
+    if x < x1 {
+        x1
+    } else if x > x2 {
+        x2
+    } else {
+        x
+    }
+}
+
+fn by_ref(x: &i32) -> i32{
+    *x + 1
+}
+
+fn main() {
+    let i = 10;
+    let res1 = by_ref(&i);
+    let res2 = by_ref(&41);
+    println!("{} {} {}", res1,res2,i);
+}
+// 11 42
